f5f609d82af8ddd6aa0351a232b45a5efeec0c24
FLXwkg <ewenheas13@gmail.com>
2026-05-06 12:06:56 +0200

    :art: Ajout powerUp(donut)

---
+++ Assets/Code/PowerUp.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public float duration = 3f;
+    public float factor = 2f;
+    public float rotateSpeed = 100f;
+    public float floatHeight = 0.3f;
+    public float floatSpeed = 2f;
+
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+        float newY = startPos.y + Mathf.Sin(Time.time * floatSpeed) * floatHeight;
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerController>().ApplyBoost(factor, duration);
+            Destroy(gameObject);
+        }
+    }
+}
